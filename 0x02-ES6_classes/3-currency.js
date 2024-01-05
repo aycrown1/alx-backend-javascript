a9f6451062b69235539db845c9c303746f7f1153
aycrown1 <ayoadeadedamola198@gmail.com>
2024-01-05 00:53:18 +0000

    Methods, static methods, computed methods names..... MONEY

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string' || typeof name !== 'string') {
+      throw new Error('Invalid input types');
+    }
+    this._code = code;
+    this._name = name;
+  }
+
+  // Getter and Setter for currency code
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw new Error('Code must be a string');
+    }
+    this._code = newCode;
+  }
+
+  // Getter and Setter dor name attributes
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  // Method to display full currency information
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
