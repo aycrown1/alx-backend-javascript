9ce40a242b966674a50314bc9b7cbeb312e579a7
aycrown1 <ayoadeadedamola198@gmail.com>
2024-01-05 01:27:50 +0000

    Inheritance

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,22 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+
+    if (typeof floors !== 'number') {
+      throw new Error('floors must be a number');
+    }
+    this._floors = floors;
+  }
+
+  // Getter for floors attribute.
+  get floors() {
+    return this._floors;
+  }
+
+  // Override the evacuationWarningMessage method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
