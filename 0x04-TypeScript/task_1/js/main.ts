35321103b230e1a4b4675172ad80a2c14c52b8c2
aycrown1 <ayoadeadedamola198@gmail.com>
2024-01-10 22:48:51 +0000

    Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,8 +1,7 @@
 // A build for the Teacher interface
 interface Teacher {
   readonly firstName: string; // can only be modified at first initialized.
-  readonly lastName: string; // can only be modified at f\
-irst initialized.
+  readonly lastName: string; // can only be modified at first initialized.
   fullTimeEmployee: boolean;
   yearsOfExperience?: number; // Optional
   location: string;
@@ -18,4 +17,33 @@ interface Directors extends Teacher {
 // Printing teachers
 function printTeacher(firstName: string, lastName: string) {
     return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+
+
+// Writing a class
+interface StudentClass {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentClass;
+}
+
+class StudentClass implements StudentClass {
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+    return "Currently working";
+   }
+
+    displayName(): string {
+    return this.firstName;
+   }
 }
