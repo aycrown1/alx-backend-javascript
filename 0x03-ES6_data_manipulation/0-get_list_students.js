6f85c441b13c481b7b1421d8f0570ef8112f0b44
aycrown1 <ayoadeadedamola198@gmail.com>
2024-01-13 01:46:18 +0000

    Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+// Create a function named getListStudents
+// that returns an array of objects.
+function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
+}
+
+export default getListStudents;
