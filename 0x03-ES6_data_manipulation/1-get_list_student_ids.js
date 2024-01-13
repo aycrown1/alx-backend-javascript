3e6c5b4162a7daaf29f8a315f5ae2954da00f73a
aycrown1 <ayoadeadedamola198@gmail.com>
2024-01-13 01:49:34 +0000

    More mapping

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,11 @@
+// Create a function getListStudentIds
+// that returns an array of ids from a list of object.
+function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  return students.map(student => student.id);
+}
+
+export default getListStudentIds;
