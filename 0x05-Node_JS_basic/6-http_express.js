de9d21ce94609c68e055a4cd0d919a0067d4a4ef
aycrown1 <ayoadeadedamola198@gmail.com>
2024-02-28 04:20:05 +0000

    Create a small HTTP server using Express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -7,8 +7,4 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
-
 module.exports = app;
