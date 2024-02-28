1da911f0c23e2fece4ea91e3d9257085d5538a86
aycrown1 <ayoadeadedamola198@gmail.com>
2024-02-28 03:35:13 +0000

    Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+function countStudents(database) {
+  if (!fs.existsSync(database)) {
+    throw new Error('Cannot load the database');
+  }
+  const data = fs.readFileSync('./database.csv', { encoding: 'utf8', flag: 'r' });
+  const headers = data.trim().split('\n')[0].trim().split(',');
+  const list = data.trim().split('\n');
+
+  const array = [];
+  const cs = [];
+  const swe = [];
+
+  for (let i = 1; i < list.length; i += 1) {
+    const cont = data.trim().split('\n')[i].trim().split(',');
+    const newarray = {};
+    for (let j = 0; j < headers.length; j += 1) {
+      newarray[headers[j]] = cont[j];
+    }
+    if (newarray.field === 'CS') {
+      cs.push(newarray.firstname);
+    }
+    if (newarray.field === 'SWE') {
+      swe.push(newarray.firstname);
+    }
+    array.push(newarray);
+  }
+  console.log(`Number of students: ${array.length}`);
+  console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+  console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+}
+module.exports = countStudents;
